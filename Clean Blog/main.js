b21e839f6837bc9e2da910c61d24f4565ccfc89f
muhammedzahit <55826843+muhammedzahit@users.noreply.github.com>
2021-10-20 01:21:17 +0300

    added static and view files

--- Clean Blog/main.js
+++ Clean Blog/main.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
+
 app.get('/', function (req, res) {
-    let blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-    res.send(blog)
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get('/index.html', function (req, res) {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get('/about.html', function (req, res) {
+    res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get('/blog_post.html', function (req, res) {
+    res.sendFile(__dirname + "/views/blog_post.html")
+})
+
+app.get('/contact.html', function (req, res) {
+    res.sendFile(__dirname + "/views/contact.html")
+})
+
+app.get('/portfolio.html', function (req, res) {
+    res.sendFile(__dirname + "/views/portfolio.html")
 })
 
 app.listen(3000)
