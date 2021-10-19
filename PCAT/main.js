d908469660636ac09a13b8a80b2603a85b19743f
muhammedzahit <55826843+muhammedzahit@users.noreply.github.com>
2021-10-20 01:15:12 +0300

    added view files

--- PCAT/main.js
+++ PCAT/main.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
- 
+
 app.use(express.static("public"))
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/temp/index.html")
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/view/index.html")
 })
- 
+
+app.get("/index.html", function (req, res) {
+  res.sendFile(__dirname + "/view/index.html")
+})
+
+app.get("/about.html", function (req, res) {
+  res.sendFile(__dirname + "/view/about.html")
+})
+
+app.get("/contact.html", function (req, res) {
+  res.sendFile(__dirname + "/view/contact.html")
+})
+
+app.get("/video-page.html", function (req, res) {
+  res.sendFile(__dirname + "/view/video-page.html")
+})
+
+
+
 app.listen(3000)
