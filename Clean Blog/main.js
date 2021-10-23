b5c105279760691949af29b7b9de0f04734c395a
muhammedzahit <55826843+muhammedzahit@users.noreply.github.com>
2021-10-23 19:41:18 +0300

    blog post connected to database

--- Clean Blog/main.js
+++ Clean Blog/main.js
@@ -1,30 +1,120 @@
 const express = require('express')
 const app = express()
 
+// ejs settings
+const ejs = require("ejs")
+app.set("view engine", "ejs")
+
+// database connection
+let MongoClient = require('mongodb').MongoClient;
+const { restart } = require('nodemon');
+let url = "mongodb+srv://admin:uzunburunmurat@cluster0.suejd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+let db;
+// Initialize connection once
+
 app.use(express.static("public"))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+let TEXT
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/views/index.html")
+    let texts
+    let link
+    const promise = new Promise(function (resolve, reject) {
+        if (typeof db === "undefined") {
+            MongoClient.connect(url, function (err, database) {
+                if (err) throw err;
+                db = database;
+                console.log("database connected")
+                resolve("cozuldu1")
+            });
+        } else {
+            resolve("cozuldu2")
+        }
+    }).then(message => {
+        return new Promise((resolve, reject) => {
+            let dbo = db.db("test-blog");
+
+            dbo.collection("texts").find({}).toArray(function (err, result) {
+                if (err) throw err;
+                texts = result
+                link = []
+                for (let i = 0; i < result.length; i++) {
+                    link.push("blog_post_" + String(i))
+                }
+                resolve("a")
+            });
+        })
+    }).then(message => {
+
+
+
+        res.render("index", {
+            texts: texts,
+            link: link
+        })
+    })
+
 })
 
 app.get('/index.html', function (req, res) {
-    res.sendFile(__dirname + "/views/index.html")
+    res.redirect("/")
 })
 
 app.get('/about.html', function (req, res) {
-    res.sendFile(__dirname + "/views/about.html")
-})
-
-app.get('/blog_post.html', function (req, res) {
-    res.sendFile(__dirname + "/views/blog_post.html")
+    res.render("about")
 })
 
 app.get('/contact.html', function (req, res) {
-    res.sendFile(__dirname + "/views/contact.html")
+    res.render("contact")
+})
+
+app.get("/blog_post_*", function (req, res) {  
+    
+    let index = req.originalUrl.charAt(req.originalUrl.length - 1)
+    console.log(isNaN(index), index)
+    const promise = new Promise(function (resolve, reject) {
+        if(isNaN(index)){
+            reject("database baglanma")
+        }
+        let dbo = db.db("test-blog");
+        console.log("i", req.originalUrl)
+        dbo.collection("texts").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            TEXT = result
+            console.log(TEXT)
+            resolve("cozuldu")
+        });
+    })
+    promise.then(message => {
+        index = Number(index)
+        console.log("text"  ,TEXT[Number(index)])
+        res.render("blog_post", {
+            text: TEXT[index]
+        })
+    })
 })
 
 app.get('/portfolio.html', function (req, res) {
-    res.sendFile(__dirname + "/views/portfolio.html")
+    res.render("portfolio")
+})
+
+app.post("/new_story", function (req, res) {
+    const promise = new Promise((resolve, reject) => {
+        let dbo = db.db("test-blog");
+        dbo.collection("texts").insertOne(req.body, function (err, res) {
+            if (err) throw err;
+            console.log("1 text inserted");
+            resolve("OK")
+        });
+    })
+    promise.then(message => {
+        res.redirect("/")
+    })
+
 })
 
 app.listen(3000)
